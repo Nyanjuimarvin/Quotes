8bc8916553af22b8e1a8165499f503f522cc53f8
Nyanjuimarvin <marnyanjui@gmail.com>
2022-02-06 09:28:21 +0300

    Display Quote sample and Add Upvote and Downvote button logic

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -6,8 +6,23 @@ import { Quotes } from '../Classes/quotes';
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
+
+
+
 export class QuotesComponent implements OnInit {
 
+  plusUp(index:number){
+    this.quotes[index].upvoteCount++;
+  }
+  plusDown(index:number){
+    this.quotes[index].downvoteCount++;
+  }
+
+  quotes:Quotes[] = [
+    new Quotes("Marvin","Novik","They are Rage, Brutal, without Mercy. But you, you will be Worse. RIP AND TEAR UNTIL ITS DONE!!!","ligma"),
+
+
+  ];
   constructor() { }
 
   ngOnInit(): void {
