8f5bfbea1d09aa1f50813eeb6cb6bdf963c83a9a
Nyanjuimarvin <marnyanjui@gmail.com>
2022-02-06 12:17:06 +0300

    Add flex layout for quotedisplay component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuotedetailsComponent } from './quotedetails/quotedetails.component';
+import { QuoteformComponent } from './quoteform/quoteform.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuotedetailsComponent
+    QuotedetailsComponent,
+    QuoteformComponent
   ],
   imports: [
     BrowserModule,
